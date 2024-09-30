90f69a57460ef0da85a07843ef220e50f336529c
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-09-30 11:58:22 +0700

    add a simple http server to handle 'get' request

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (ServerSocket serverSocket = new ServerSocket(8000)) {
+            System.out.println("Server is running on http://localhost:" + serverSocket.getLocalPort());
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                handleRequest(clientSocket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleRequest(Socket clientSocket) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+            
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                if (inputLine.startsWith("GET")) {
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Content-Type: text/html");
+                    out.println();
+                    out.println("""
+                        <html>
+                            <head>
+                                <title>Welcome!</title>
+                            </head>
+                            <body>
+                                <h1>Welcome! You successfully get response from server</h1>
+                            </body>
+                        </html>
+                        """
+                    );
+                    System.out.println("Response success !\n\n");
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
