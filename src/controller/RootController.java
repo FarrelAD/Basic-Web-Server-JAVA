01b75776116d055cd30a71efef1c65e5af88b17b
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-10-20 11:29:46 +0700

    fix html code format in root page

--- src/controller/RootController.java
+++ src/controller/RootController.java
@@ -20,24 +20,30 @@ public class RootController {
             out.println();
             out.println("""
                 <html>
-                    <head>
-                        <title>Root Page</title>
-                    </head>
-                    <body>
-                        <h1>Welcome! You successfully get response from server</h1>
-                        <p>Current user: """+data.getArrayLength()+"""
-                        </p>
-                        <br>
-                        <div>
-                            <h1>Send user data to server!</h1>
-                            <form action="/users" method="post">
-                                <input type="text" name="name" id="name-input" placeholder="Input name"> 
-                                <br>
-                                <input type="text" name="job" id="job-input" placeholder="Input job">
-                                <br>
-                                <button type="submit">Submit</button>
-                            </form>
-                        </div>
+
+                <head>
+                    <title>Root Page</title>
+                </head>
+
+                <body>
+                    <h1>Welcome! You successfully get response from server</h1>
+                    <p>Current user: """+data.getArrayLength()+"""
+                    </p>
+                    
+                    <br>
+
+                    <div>
+                        <h1>Send user data to server!</h1>
+                        <form action="/users" method="post">
+                            <input type="text" name="name" id="name-input" placeholder="Input name"> 
+                            <br>
+                            <input type="text" name="job" id="job-input" placeholder="Input job">
+                            <br>
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                    
+                    <div>
                         <h1>Search user data!</h1>
                         <form action="/search" method="get">
                             <input type="text" name="name" id="search-box-name" placeholder="Name">
@@ -47,7 +53,9 @@ public class RootController {
                             <button type="submit">Search</button>
                         </form>
                         <p><i>*) Empty search box will get all user data</i></p>
-                    </body>
+                    </div>
+                </body>
+
                 </html>
             """);
         } catch (Exception e) {
